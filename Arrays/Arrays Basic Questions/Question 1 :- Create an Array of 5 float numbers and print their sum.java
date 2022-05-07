2ceac7af9a18bd6d44f90df16d651647e28ff356
SagarAkhade <104863414+SagarAkhade@users.noreply.github.com>
2022-05-08 00:20:51 +0530

    Create  Question 1 :- Create an Array of 5 float numbers and print their sum.java

---
+++ Arrays/Arrays Basic Questions/Question 1 :- Create an Array of 5 float numbers and print their sum.java
@@ -0,0 +1,10 @@
+// Question 1 :- Create an Array of 5 float numbers and print their sum
+  float[] marks = {10.0f, 20.0f, 30.0f, 40.0f, 50.0f};
+  float sum = 0.0f;
+  for(int i=0; i<marks.length; i++){
+     sum = sum + marks[i];
+  }
+  System.out.println("The value of sum is: "+sum);
+==========================
+Output :-
+The value of sum is: 150.0
