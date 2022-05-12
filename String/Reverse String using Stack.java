7feacaf9cb0f703c8779fcd5c911efe8208d5563
Sagar <104863414+SagarAkhade@users.noreply.github.com>
2022-05-12 15:41:29 +0530

    Create Reverse String using Stack.java

---
+++ String/Reverse String using Stack.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.*;
+
+// Q. Reverse String using Stack | Coding Interview Question 
+public class A_4_String_2_Reverse_String_using_Stack {
+    public static void main(String[] args) {
+        String str = "ABCD";
+        System.out.println("Before reverse : " + str);
+        System.out.println("After reverse : " + reverseString(str));
+    }
+
+    public static String reverseString(String str){
+        char[] chars = str.toCharArray();
+        Stack<Character> stack = new Stack<>(); // Stack will store string -> FILO
+        for(char c : chars){
+            stack.push(c);  // stack.push() method will add elements in stack
+        }
+        for(int i=0; i<str.length(); i++){
+            chars[i] = stack.pop();  //stack.pop(); method will gives us elements ba
+        }
+        return new String(chars);
+    }
+}
+----------------------------
+Output - 
+Before reverse : ABCD
+After reverse : DCBA
