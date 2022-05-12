cf20d90aeaaf56ac54e1d7446b123f278497c8de
Sagar <104863414+SagarAkhade@users.noreply.github.com>
2022-05-12 15:42:40 +0530

    Create First Non Repeating Character of String.java

---
+++ String/First Non Repeating Character of String.java
@@ -0,0 +1,31 @@
+package com.company;
+import java.util.HashMap;
+import java.util.*;
+
+// Q. First Non Repeating Character of String | Coding Interview Question
+public class A_4_String_4_First_Non_Repeating_Character_In_String {
+    public static void main(String[] args) {
+        String str = "codeforcode";
+        //String str = "aabb";
+        System.out.println(firstNonRepeatingCharacter(str));
+    }
+
+		//Optimal Solution - Time Complexity -> O(N) Space Complexity -> O(N)
+    public static int firstNonRepeatingCharacter(String str){
+        Map<Character, Integer> map = new HashMap<>();
+        char[] chars = str.toCharArray();
+        for(char ch : chars){ // putting characters in string with frequency
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for(int i=0; i<chars.length; i++){  // Getting Index of first non-repeating character in String
+            char ch = chars[i];
+            if(map.get(ch)==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+----------------
+Output - 
+4
