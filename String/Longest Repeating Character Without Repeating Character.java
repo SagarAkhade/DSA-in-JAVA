5e6431a9c1194c05710f5bb5e45aa90c1a16c5c9
Sagar <104863414+SagarAkhade@users.noreply.github.com>
2022-05-12 15:43:45 +0530

    Create Longest Repeating Character Without Repeating Character.java

---
+++ String/Longest Repeating Character Without Repeating Character.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.*;
+
+// Q. Longest Repeating Character Without Repeating Character | Coding Interview Question
+public class A_4_String_6_Longest_SubString_Without_Repeating_Character {
+    public static void main(String[] args) {
+        String str = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(str));
+    }
+
+    // Optimal Solution - Time Complexity -> O(N) Space Complexity -> O(N)
+    public static int lengthOfLongestSubstring(String str){
+        Map<Character, Integer> map = new HashMap<>();
+        int maxLength = 0;
+        int start = 0;
+        for(int end=0; end<str.length(); end++){
+            char rightChar = str.charAt(end);
+            if(map.containsKey(rightChar)){ // for increasing start value if we get repeating character
+                start = Math.max(start, map.get(rightChar) + 1);
+            }
+            map.put(rightChar, end);
+            maxLength = Math.max(maxLength, end - start + 1);
+        }
+        return maxLength;
+    }
+}
+---------------
+Output - 
+3
