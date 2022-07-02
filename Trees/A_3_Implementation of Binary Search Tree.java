e098cafa7ffe28468c8b988240802797faf9806b
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-07-02 23:06:31 +0530

    Create A_3_Implementation of Binary Search Tree.java

---
+++ Trees/A_3_Implementation of Binary Search Tree.java
@@ -0,0 +1,20 @@
+package com.company.A_9_Trees;
+// Implementation of Binary Search Tree
+
+public class A_9_Binary_Tree_3_Implementing_Binary_Search_Tree {
+    public TreeNode root;  //instance variable for BST
+    public class TreeNode{  //inner class for BST
+        int data;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode(int data) { // Constructor for BST
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
