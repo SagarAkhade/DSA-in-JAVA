b89bc47a651e24625f68ff990a0c34e36c07a282
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-08-26 16:38:18 +0530

    Update 01_Fibonacci_Number_Using_DP.java

--- Dynamic Programing/01_Fibonacci_Number_Using_DP.java
+++ Dynamic Programing/01_Fibonacci_Number_Using_DP.java
@@ -3,6 +3,7 @@ package com.company.A_11_Dynamic_Programing;
 //Input: 6   Output: 8
 //Fibonacci Series : 0 1 1 2 3 5 8 13 21 ....
 public class A_01_Fibonacci_Number_Using_DP {
+    //Bottom Down Approach of DP
     public static int fibonacciNumber(int n) {
         int[] table = new int[n + 1];
         table[0] = 0;
@@ -12,8 +13,28 @@ public class A_01_Fibonacci_Number_Using_DP {
         }
         return table[n];
     }
+    //Top Down Approach of DP
+    public static int fib(int[] memo, int n) {
+        if(memo[n] == 0) {
+            if(n < 2) {
+                memo[n] = n;
+            }
+            else {
+                int left = fib(memo, n - 1);
+                int right = fib(memo, n - 2);
+                memo[n] = left + right;
+            }
+        }
+        return memo[n];
+    }
+
     public static void main(String[] args) {
+        //Bottom Down Approach of DP
         System.out.println(fibonacciNumber(6));
+        //Output: 8
+
+        //Top Down Approach of DP
+        System.out.println(fib(new int[6 + 1], 6));
         //Output: 8
     }
 }
