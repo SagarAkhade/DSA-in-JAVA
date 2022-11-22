d1a96ba909972473a72084aefdd9048ef73fe9bb
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-11-22 18:57:28 +0530

    Create 04. Handling_Specific_Exception.java

---
+++ A_01 Errors & Exceptions in Java/04. Handling_Specific_Exception.java
@@ -0,0 +1,33 @@
+package com.company.Ch_14_Errors_And_Exceptions;
+import java.util.*;
+
+public class Lec81_Handling_Specific_Exception {
+    public static void main(String[] args) {
+        int [] marks = new int[3];
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[2] = 6;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array index");
+        int ind = sc.nextInt();
+
+        System.out.println("Enter the number you want to divide the value with");
+        int number = sc.nextInt();
+        try{
+            System.out.println("The value at array index entered is: " + marks[ind]);
+            System.out.println("The value of array-value/number is: " + marks[ind]/number);
+        }
+        catch (ArithmeticException e){
+            System.out.println("ArithmeticException occured!");
+            System.out.println(e);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("ArrayIndexOutOfBoundsException occured!");
+            System.out.println(e);
+        }
+        catch (Exception e){
+            System.out.println("Some other exception occured!");
+            System.out.println(e);
+        }
+    }
+}
