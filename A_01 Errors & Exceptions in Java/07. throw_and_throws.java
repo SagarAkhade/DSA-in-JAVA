dcdf105c58a884409d457534aa55cc0f24592d7b
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-11-22 19:50:26 +0530

    Create 07. throw_and_throws.java

---
+++ A_01 Errors & Exceptions in Java/07. throw_and_throws.java
@@ -0,0 +1,42 @@
+package com.company.Ch_14_Errors_And_Exceptions;
+
+class NegativeRadiusException extends Exception{
+    @Override
+    public String toString() {
+        return "Radius cannot be negative!";
+    }
+
+    @Override
+    public String getMessage() {
+        return "Radius cannot be negative!";
+    }
+}
+
+public class Lec84_throw_and_throws {
+
+    public static double area(int r) throws NegativeRadiusException{
+        if (r<0){
+            throw new NegativeRadiusException();
+        }
+        double result = Math.PI * r * r;
+        return result;
+    }
+
+    public static int divide(int a, int b) throws ArithmeticException{
+        // Made By Harry
+        int result = a/b;
+        return result;
+    }
+    public static void main(String[] args) {
+        // Shivam - uses divide function created by Harry
+        try{
+//            int c = divide(6, 0);
+//            System.out.println(c);
+            double ar = area(6);
+            System.out.println(ar);
+        }
+        catch(Exception e){
+            System.out.println("Exception");
+        }
+    }
+}
