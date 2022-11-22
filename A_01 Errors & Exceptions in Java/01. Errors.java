c30e00de5ea9d2cbfb36670be5b5e94005a01c1d
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-11-22 18:22:10 +0530

    Create 01. Errors.java

---
+++ A_01 Errors & Exceptions in Java/01. Errors.java
@@ -0,0 +1,13 @@
+package com.company.Ch_14_Errors_And_Exceptions;
+// Types of Errors:
+// 1) Syntax Error:- When compiler finds something wrong with program it throws Syntax Error
+// 2) Logical Error(Bugs) :- Logical Errors come when some bugs happen in Code
+// 3) Run time Error(Also called Exceptions):- These are basically if user put wrong inputs or the network issues
+
+public class Lec78_Errors {
+
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+    }
+}
