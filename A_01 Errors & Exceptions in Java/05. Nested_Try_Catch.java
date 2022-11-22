1a48f2de693b612a84760bd4154cee735e5378c9
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-11-22 19:06:28 +0530

    Create 05. Nested_Try_Catch.java

---
+++ A_01 Errors & Exceptions in Java/05. Nested_Try_Catch.java
@@ -0,0 +1,31 @@
+package com.company.Ch_14_Errors_And_Exceptions;
+
+import java.util.Scanner;
+
+public class Lec82_Nested_Try_Catch {
+    public static void main(String[] args) {
+        int [] marks = new int[3];
+        marks[0] = 7;
+        marks[1] = 56;
+        marks[2] = 6;
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        while(flag) {
+            System.out.println("Enter the value of index");
+            int ind = sc.nextInt();
+            try {
+                System.out.println("Welcome to video no 82");
+                try {
+                    System.out.println(marks[ind]);
+                    flag = false;
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Sorry this index does not exist");
+                    System.out.println("Exception in level 2");
+                }
+            } catch (Exception e) {
+                System.out.println("Exception in level 1");
+            }
+        }
+        System.out.println("Thanks for using this program");
+    }
+}
