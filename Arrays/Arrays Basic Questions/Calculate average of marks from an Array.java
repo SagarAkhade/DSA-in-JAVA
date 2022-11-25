c4ff4a82902d5c8f34c900be96b77be3b4610531
Sagar Akhade <104863414+SagarAkhade@users.noreply.github.com>
2022-11-25 12:22:18 +0530

    Update Calculate average of marks from an Array.java

--- Arrays/Arrays Basic Questions/Calculate average of marks from an Array.java
+++ Arrays/Arrays Basic Questions/Calculate average of marks from an Array.java
@@ -1,7 +1,7 @@
 // Question 3:- Calculate average of marks from an Array
 float[] marks = { 88, 95, 55, 96, 98};
 float sum = 0f;
-for(int i = 0; i<marks.length; i++){
+for(int i = 0; i<marks.length; i++){ 
    sum =sum + marks[i];
 }
 float avg = sum/marks.length;
